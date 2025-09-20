ac6c1a91eb6fcad5b6da7690e3d61be43ed03380
CHOIJIONE\choij <choijione02@gmail.com>
2025-09-20 18:46:41 +0900

    메인 루프에 플레이어 액터 추가

--- Game/Source/Src/Main.cpp
+++ Game/Source/Src/Main.cpp
@@ -1,5 +1,7 @@
 #include <raylib.h>
 
+#include "Player.h"
+
 int main(void)
 {
     const int screenWidth = 800;
@@ -7,14 +9,25 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "Avoid");
     SetTargetFPS(60);
     
+    std::unique_ptr<Player> player = std::make_unique<Player>();
+    player->OnCreate();
+
     while (!WindowShouldClose())
     {
         float deltaSeconds = GetFrameTime();
 
+        player->OnUpdate(deltaSeconds);
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        {
+            ClearBackground(RAYWHITE);
+            player->OnRender();
+        }
         EndDrawing();
     }
+
+    player->OnDestroy();
+    player.reset();
 
     CloseWindow();
     return 0;
