66e5021fbf7c76a69b91e3044f020ee56d61ebcd
CHOIJIONE\choij <choijione02@gmail.com>
2025-09-20 18:45:40 +0900

    플레이어의 model에 색상 추가

--- Game/Source/Inc/PlayerModel.h
+++ Game/Source/Inc/PlayerModel.h
@@ -8,7 +8,7 @@ class PlayerModel : public IModel
 {
 public:
 	PlayerModel() = default;
-	PlayerModel(const Vector2& center, const Vector4& color, const float& radius)
+	PlayerModel(const Vector2& center, const Color& color, const float& radius)
 		: _center(center), _color(color), _radius(radius) {}
 	PlayerModel(PlayerModel&& instance) noexcept
 		: _center(instance._center), _color(instance._color), _radius(instance._radius) {}
@@ -46,15 +46,15 @@ public:
 	}
 
 	const Vector2& GetCenter() const { return _center; }
-	const Vector4& GetColor() const { return _color; }
+	const Color& GetColor() const { return _color; }
 	const float& GetRadius() const { return _radius; }
 
 	void SetCenter(const Vector2& center) { _center = center; }
-	void SetColor(const Vector4& color) { _color = color; }
+	void SetColor(const Color& color) { _color = color; }
 	void SetRadius(const float& radius) { _radius = radius; }
 
 private:
 	Vector2 _center = Vector2{0.0f, 0.0f};
-	Vector4 _color = Vector4{ 1.0f, 1.0f, 1.0f, 1.0f };
-	float _radius = 10.0f;
+	Color _color = Color{ 0, 0, 0, 255 };
+	float _radius = 100.0f;
 };
