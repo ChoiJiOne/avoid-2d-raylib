b0f45ed2e7e7a3ec9929ec870c4a3aad83bd2b16
CHOIJIONE\choij <choijione02@gmail.com>
2025-09-20 18:54:52 +0900

    플레이어의 입력 방향에 따라 움직이는 기능 추가

--- Game/Source/Inc/PlayerModel.h
+++ Game/Source/Inc/PlayerModel.h
@@ -8,12 +8,12 @@ class PlayerModel : public IModel
 {
 public:
 	PlayerModel() = default;
-	PlayerModel(const Vector2& center, const Color& color, const float& radius)
-		: _center(center), _color(color), _radius(radius) {}
+	PlayerModel(const Vector2& center, const Color& color, const float& radius, const float& speed)
+		: _center(center), _color(color), _radius(radius), _speed(speed){}
 	PlayerModel(PlayerModel&& instance) noexcept
-		: _center(instance._center), _color(instance._color), _radius(instance._radius) {}
+		: _center(instance._center), _color(instance._color), _radius(instance._radius), _speed(instance._speed) {}
 	PlayerModel(const PlayerModel& instance) noexcept
-		: _center(instance._center), _color(instance._color), _radius(instance._radius) {}
+		: _center(instance._center), _color(instance._color), _radius(instance._radius), _speed(instance._speed) {}
 
 	virtual ~PlayerModel() = default;
 
@@ -27,6 +27,7 @@ public:
 		_center = instance._center;
 		_color = instance._color;
 		_radius = instance._radius;
+		_speed = instance._speed;
 		
 		return *this;
 	}
@@ -41,6 +42,7 @@ public:
 		_center = instance._center;
 		_color = instance._color;
 		_radius = instance._radius;
+		_speed = instance._speed;
 
 		return *this;
 	}
@@ -48,13 +50,16 @@ public:
 	const Vector2& GetCenter() const { return _center; }
 	const Color& GetColor() const { return _color; }
 	const float& GetRadius() const { return _radius; }
+	const float& GetSpeed() const { return _speed; }
 
 	void SetCenter(const Vector2& center) { _center = center; }
 	void SetColor(const Color& color) { _color = color; }
 	void SetRadius(const float& radius) { _radius = radius; }
+	void SetSpeed(const float& speed) { _speed = speed; }
 
 private:
 	Vector2 _center = Vector2{0.0f, 0.0f};
-	Color _color = Color{ 0, 0, 0, 255 };
-	float _radius = 100.0f;
+	Color _color = Color{ 255, 150, 100, 255 };
+	float _radius = 20.0f;
+	float _speed = 300.0f;
 };
--- Game/Source/Src/PlayerController.cpp
+++ Game/Source/Src/PlayerController.cpp
@@ -1,5 +1,30 @@
 #include "PlayerController.h"
+#include "PlayerModel.h"
 
 void PlayerController::Update(IModel& model, float deltaSeconds)
 {
+	PlayerModel& playerModel = static_cast<PlayerModel&>(model);
+    Vector2 center = playerModel.GetCenter();
+    
+    if (IsKeyDown(KEY_RIGHT))
+    {
+        center.x += deltaSeconds * playerModel.GetSpeed();
+    }
+
+    if (IsKeyDown(KEY_LEFT))
+    {
+        center.x -= deltaSeconds * playerModel.GetSpeed();
+    }
+
+    if (IsKeyDown(KEY_UP))
+    {
+        center.y -= deltaSeconds * playerModel.GetSpeed();
+    }
+
+    if (IsKeyDown(KEY_DOWN))
+    {
+        center.y += deltaSeconds * playerModel.GetSpeed();
+    }
+
+    playerModel.SetCenter(center);
 }
