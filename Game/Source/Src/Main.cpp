2ec7d6e6fb08c4dc992069a290d3a7c3d7bc4e64
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-09-20 12:23:55 +0900

    아무 동작도 수행하지 않는 게임 루프 추가

--- Game/Source/Src/Main.cpp
+++ Game/Source/Src/Main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <raylib.h>
 
-int main(int argc, char* argv[])
+int main(void)
 {
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Avoid");
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
