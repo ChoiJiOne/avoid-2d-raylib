29a924b083c1253291b154ec37b077c02fd56ca7
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-09-21 11:59:18 +0900

    페이즈를 생성/파괴 및 관리하는 매니저 추가

---
+++ Game/Source/Inc/PhaseManager.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "IPhase.h"
+#include "IManager.h"
+
+class PhaseManager : public IManager<PhaseManager>
+{
+public:
+    DISALLOW_COPY_AND_ASSIGN(PhaseManager);
+
+    void Startup();
+    virtual void Shutdown() override;
+
+    template <typename TPhase, typename... Args>
+    TPhase* Create(Args&&... args)
+    {
+        int32_t phaseID = -1;
+        for (size_t idx = 0; idx < _phasePool.size(); ++idx)
+        {
+            if (!_phasePool[idx].phase && !_phasePool[idx].isOccupied)
+            {
+                phaseID = idx;
+                break;
+            }
+        }
+
+        if (phaseID == -1)
+        {
+            _phasePool.emplace_back();
+            phaseID = static_cast<int32_t>(_phasePool.size()) - 1;
+        }
+
+        _phasePool[phaseID].isOccupied = true;
+        _phasePool[phaseID].phase = std::make_unique<TPhase>(args...);
+
+        return static_cast<TPhase*>(_phasePool[phaseID].phase.get());
+    }
+
+    void Destroy(const IPhase* phase);
+
+    void Register(const std::string& name, IPhase* phase);
+    void Unregister(const std::string& name);
+    IPhase* GetRegisteredPhase(const std::string& name);
+
+private:
+    friend class IManager<PhaseManager>;
+
+    PhaseManager() = default;
+    virtual ~PhaseManager() = default;
+
+    struct PhasePoolSlot
+    {
+        std::unique_ptr<IPhase> phase;
+        bool isOccupied = false;
+    };
+
+private:
+    std::vector<PhasePoolSlot> _phasePool;
+    std::map<std::string, IPhase*> _phaseCacheMap;
+};
---
+++ Game/Source/Src/PhaseManager.cpp
@@ -0,0 +1,81 @@
+#include "GameAssert.h"
+#include "GameLogging.h"
+#include "PhaseManager.h"
+
+void PhaseManager::Startup()
+{
+	if (_isInitialized)
+	{
+		GAME_LOG_ERR("ALREADY_INITIALIZE_PHASE_MANAGER");
+		return;
+	}
+
+	_isInitialized = true;
+}
+
+void PhaseManager::Shutdown()
+{
+	if (!_isInitialized)
+	{
+		GAME_LOG_ERR("NOT_INITIALIZE_PHASE_MANAGER");
+		return;
+	}
+
+	for (size_t idx = 0; idx < _phasePool.size(); ++idx)
+	{
+		if (_phasePool[idx].phase)
+		{
+			_phasePool[idx].phase.reset();
+			_phasePool[idx].isOccupied = false;
+		}
+	}
+
+	_isInitialized = false;
+}
+
+void PhaseManager::Destroy(const IPhase* phase)
+{
+	int32_t phaseID = -1;
+	for (size_t idx = 0; idx < _phasePool.size(); ++idx)
+	{
+		if (phase == _phasePool[idx].phase.get())
+		{
+			phaseID = static_cast<int32_t>(idx);
+			break;
+		}
+	}
+
+	if (phaseID != -1 && _phasePool[phaseID].phase)
+	{
+		_phasePool[phaseID].phase.reset();
+		_phasePool[phaseID].isOccupied = false;
+	}
+}
+
+void PhaseManager::Register(const std::string& name, IPhase* phase)
+{
+	auto it = _phaseCacheMap.find(name);
+	GAME_ASSERT(it == _phaseCacheMap.end(), "ALREADY_REGISTER_PHASE(name:%s)", name.c_str());
+
+	_phaseCacheMap.insert({ name, phase });
+}
+
+void PhaseManager::Unregister(const std::string& name)
+{
+	auto it = _phaseCacheMap.find(name);
+	GAME_ASSERT(it != _phaseCacheMap.end(), "ALREADY_UNREGISTER_PHASE_OR_NOT_REGISTER(name:%s)", name.c_str());
+
+	_phaseCacheMap.erase(it);
+}
+
+IPhase* PhaseManager::GetRegisteredPhase(const std::string& name)
+{
+	auto it = _phaseCacheMap.find(name);
+	if (it == _phaseCacheMap.end())
+	{
+		GAME_LOG_ERR("FAILED_TO_GET_REGISTERED_PHASE(name:%s)", name.c_str());
+		return nullptr;
+	}
+
+	return it->second;
+}
