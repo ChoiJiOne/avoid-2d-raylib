1446d6ef81913804a492e619cae5ffe1e5058e86
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-09-22 09:18:35 +0900

    적 액터를 생성하는 스포너 컨트롤러에서 생성 시간 갱신 기능 추가

--- Game/Source/Src/EnemySpawnerController.cpp
+++ Game/Source/Src/EnemySpawnerController.cpp
@@ -1,4 +1,5 @@
 #include "EnemySpawnerController.h"
+#include "EnemySpawnerModel.h"
 
 void EnemySpawnerController::PreUpdate(IModel& model, float deltaSeconds)
 {
@@ -6,6 +7,8 @@ void EnemySpawnerController::PreUpdate(IModel& model, float deltaSeconds)
 
 void EnemySpawnerController::Update(IModel& model, float deltaSeconds)
 {
+	EnemySpawnerModel& enemySpawnerModel = static_cast<EnemySpawnerModel&>(model);
+	enemySpawnerModel.TickSpawnTimer(deltaSeconds);
 }
 
 void EnemySpawnerController::PostUpdate(IModel& model, float deltaSeconds)
